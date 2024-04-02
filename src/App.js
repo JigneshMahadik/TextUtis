01a46573312f51627bba8b139d1918a7cec87180
JigneshMahadik <149412247+JigneshMahadik@users.noreply.github.com>
2024-04-02 18:03:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 // import logo from './logo.svg';
 import './App.css';
+import { About } from './components/about';
 import { Home } from './components/home';
+import { Navbar } from './components/navbar';
 // import { Navbar } from './components/navbar';
+import { Route,Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+        {/* <Navbar/> */}
+        <Routes>
+          <Route path="/"  element={<Home/>} />
+          <Route path="/About"  element={<About/>} />
+        </Routes>
+      {/* </Navbar> */}
     </div>
   );
 }
